fda3f84362899b3643c064f3ceeb96d79a45f4e0
yeasinChowdhury <depressedman996@gmail.com>
2025-06-06 23:13:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+
+
+
+
+function App() {
+ 
+
+  return (
+   <>
+   <div> <h1 className="bg-blue-400 py-[50px] text-center font-bold text-4xl m-[30px]" >wlc Japan </h1>
+           </div>
+     <Header></Header>
+ 
+
+   </>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div >
+      Headerty45et
+    </div>
+  )
+}
+
+export default Header
